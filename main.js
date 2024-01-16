157ed93e071ed4fcdfcc69b6bc4d63f4e2d1cf3c
Josip1123 <jopetk@hotmail.com>
2024-01-16 11:52:40 +0100

    submit btn logic done

--- main.js
+++ main.js
@@ -1,25 +1,42 @@
+// SUBMIT NAME DEL
+
+// DOM elementer vi ska använda med submit btn
 const userNameSubmitBtn = document.querySelector("#btn");
 const userInput = document.querySelector("#userName");
 const welcomeText = document.querySelector(".welcome");
 
 userNameSubmitBtn.disabled = true; //btn ska vara disabled på default
 
+//två funktioner som jag kan använda flera gånger, DRY
+function enableBtn() {
+    userNameSubmitBtn.disabled = false;
+    userNameSubmitBtn.classList.remove("disabled");
+}
+function disableBtn() {
+    userNameSubmitBtn.disabled = true;
+    userNameSubmitBtn.classList.add("disabled");
+}
 
 function checkName() {
-    // om input tex har mer än 3 char då får man clicka på knappen
+    // om input text har mer än 3 char då kan man avända submit knappen
+    // anars knappen blir disablead
     if (userInput.value.length >= 3) {
-        userNameSubmitBtn.disabled = false;
-        userNameSubmitBtn.classList.remove("disabled");
+        enableBtn();
     } else {
-        userNameSubmitBtn.disabled = true;
-        userNameSubmitBtn.classList.add("disabled");
+        disableBtn();
     }
 }
 
-userInput.addEventListener("input", checkName); //funktion kollar vid varje imput om lenghten blev över 3
+userInput.addEventListener("input", checkName); //funktion kollar vid varje imput om lenghten blev > 3
 
 userNameSubmitBtn.addEventListener("click", (getName) => {
     welcomeText.textContent = `Welcome ${userInput.value} !`; //tar input value och använder den för velkomst text
     userInput.value = ""; //rensar fältet efteråt
-    checkName(); //efter vi raderar input field btn är fortfarande enabled trots tom så jag kallar funktionen att kolla om btn ska vara disabled
+    disableBtn(); //efter vi raderar input field btn är fortfarande enabled trots tom
 });
+
+//
+//
+//
+//
+//
