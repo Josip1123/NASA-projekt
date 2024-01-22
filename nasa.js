76e6ac8ff10a90997e0d1b94616cdb04ff2d8888
Josip1123 <jopetk@hotmail.com>
2024-01-22 13:39:13 +0100

    added some coments

--- nasa.js
+++ nasa.js
@@ -3,6 +3,11 @@
 const apod =
     "https://api.nasa.gov/planetary/apod?api_key=IpEsZxZCFRfdnid2KwftbKCDTtaFhjKtadTk0HzD";
 
+/*  
+    valt att använda async await eftersom jag tycker att det är lättare att läsa och fatta vad som händer
+    en smydigare sätt att skriva kod 
+*/
+
 async function fetchAstroImageOfTheDay() {
     try {
         // fetching image of the day from NASA API
@@ -10,9 +15,9 @@ async function fetchAstroImageOfTheDay() {
         if (!response.ok) {
             throw new Error(`Something went wrong...`);
         }
-        //convert response to JSON format
-        const responseJsoned = await response.json();
-        const imageOfTheDay = responseJsoned.url;
+        const responseJsoned = await response.json(); //convert response to JSON format
+        const imageOfTheDay = responseJsoned.url; //väljer bara url prop från JSON file
+        // lägger nytt url i DOM - hero bg img
         const heroElement = document.querySelector(".hero");
         heroElement.attributeStyleMap.append(
             "background-image",
